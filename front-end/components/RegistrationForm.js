830fe2628f96b6d2341351acca54c072698b2950
marnanakou <marnanakou@gmail.com>
2020-03-13 11:42:55 +0100

    files created

---
+++ front-end/components/RegistrationForm.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class RegistrationForm extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+export default RegistrationForm;
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import RegistrationForm from "./components/RegistrationForm";
 import "./App.css";
 
 function App() {
   return (
     <div className="finalProject">
       <header className="Final Project Starts!"></header>
+      <RegistrationForm />
     </div>
   );
 }
