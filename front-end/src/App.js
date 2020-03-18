367598ecc79144a4c86ebbc34e1ac4c1b1fd8ab5
123aadina <adina.hamida@gmail.com>
2020-03-18 14:33:15 +0100

    please make some css for the pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/Layout/Navbar'
+import Home from './components/Pages/Home'
+import Info from './components/Pages/Info'
+
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <header className="App-header">
+        </header>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/info' component={Info} />
+          />
+      </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ front-end/src/components/Layout/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <div className='pg-primary'>
+            <footer>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                <Link to='/info'>Info</Link>
+                </li>
+               
+                </ul>
+                footer
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
