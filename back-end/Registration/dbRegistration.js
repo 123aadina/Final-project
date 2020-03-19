c802922ff2de98e622981840ab097fdde2969089
marnanakou <marnanakou@gmail.com>
2020-03-19 14:29:38 +0100

    creating backend files

---
+++ back-end/Registration/dbRegistration.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// //setting the connection with Atlas
+// const strMongoConnection = "";
+// mongoose
+//   .connect(strMongoConnection, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => console.log("Connection worked"))
+//   .catch(err => console.log("Connection failed"));
+
+//creating the Schema models
+//name,email,phone ,password tha prepei na simperilavw ola ta pedia mou sto schema?diladi kai languages issues
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: { type: string, required: true },
+    email: { type: string, required: true },
+    password: { type: string, required: true },
+    phone: { type: string, required: true }
+  })
+);
+
+module.exports = { User };
---
+++ back-end/Registration/serverRegistration.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { User } = require("./dbRegistration");
+const { cors } = require("../middleware");
+const app = express();
+const bcrypt = require(bcrypt);
+const session = require(session);
+
+//setting the middleware
+app.use(express.json);
+app.use(session({ secret: "ssshhhhh" }));
+app.use(cors());
+
+//server listening on 8000;
+let port = 8000;
+app.listen(port, () => {
+  console.log(`Server is now listening on port ${port}`);
+});
