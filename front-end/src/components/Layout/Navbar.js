7decb7b109509c953209ab9f810dde55c6b10319
Christophe Troudart <ch.troudart@gmail.com>
2020-03-20 10:25:51 +0100

    Syntax error fixed

--- front-end/src/components/Layout/Navbar.js
+++ front-end/src/components/Layout/Navbar.js
@@ -1,53 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import { useTranslation } from 'react-i18next';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { useTranslation } from "react-i18next";
 
 const Navbar = ({ icon, title }) => {
-    const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
-    const handleOnClick = (lang) => {
-        i18n.changeLanguage(lang)
-    }
+  const handleOnClick = lang => {
+    i18n.changeLanguage(lang);
+  };
 
-    return (
-        <nav className='navbar bg-primary'>
-            <h1>
-                <i className={icon}></i>
-            </h1>
-            <button onClick={() => handleOnClick('en')}>English</button>
-            <button onClick={() => handleOnClick('de')}>Deutch</button>
-            <button onClick={() => handleOnClick('fe')}>French</button>
-            <button onClick={() => handleOnClick('fr')}>Farsi</button>
-            <button onClick={() => handleOnClick('ar')}>Arbic</button>
-            <button onClick={() => handleOnClick('tu')}>Turkish</button>
-            <h1>
-                <i className={title}></i>
-            </h1>
-            <ul>
-                <li>
-                    
-                </li>
-                <li>
-                    
-                </li>
-
-            </ul>
-        </nav>
-    )
-}
+  return (
+    <nav className="navbar bg-primary">
+      <h1>
+        <i className={icon}></i>
+      </h1>
+      <button onClick={() => handleOnClick("en")}>English</button>
+      <button onClick={() => handleOnClick("de")}>Deutsch</button>
+      <button onClick={() => handleOnClick("fe")}>French</button>
+      <button onClick={() => handleOnClick("fr")}>Farsi</button>
+      <button onClick={() => handleOnClick("ar")}>Arabic</button>
+      <button onClick={() => handleOnClick("tu")}>Turkish</button>
+      <h1>
+        <i className={title}></i>
+      </h1>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  );
+};
 
 Navbar.defaultProps = {
-    icon: 'fas fa-bars',
-    title: 'hello'
+  icon: "fas fa-bars",
+  title: "hello"
 };
 
 Navbar.propTypes = {
-    icon: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 };
-export default Navbar
+export default Navbar;
 
 //<i class="fas fa-bars"></i>
 //<Link to='/'>Home</Link>
