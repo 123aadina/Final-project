064e20444f53f4206b3f15a0643b3df016117b8e
Christophe Troudart <ch.troudart@gmail.com>
2020-03-23 12:05:06 +0100

    Navbar - Switch - Route added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 // Styles
-import "./styles/App.css";
+import "./styles/App.scss";
 
 // Components
 import React from "react";
@@ -13,12 +13,12 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
-        <header className="App-header"></header>
+        <header className="App-header">
+          <Navbar />
+        </header>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/info" component={Info} />
-          />
           <Route exact path="/registration" component={RegistrationForm} />
         </Switch>
       </div>
