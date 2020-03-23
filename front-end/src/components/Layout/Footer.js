b9d31716577fe9b9c6847983bca10c70b3eb5a1e
Christophe Troudart <ch.troudart@gmail.com>
2020-03-23 11:58:53 +0100

    Footer done

--- front-end/src/components/Layout/Footer.js
+++ front-end/src/components/Layout/Footer.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div className='pg-primary'>
-            <footer>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                <Link to='/info'>Info</Link>
-                </li>
-               
-                </ul>
-                footer
-            </footer>
-        </div>
-    )
-}
+  return (
+    <div className="homepage_footer mt-5">
+      <footer>
+        <ul>
+          <li className="mx-2 pt-3 pl-3">
+            <Link to="/" className="text-secondary">
+              {" "}
+              Home{" "}
+            </Link>
+          </li>
+          <li className="mx-2 pt-3">
+            <Link to="/info" className="text-secondary">
+              {" "}
+              Info{" "}
+            </Link>
+          </li>
+          <li className="mx-2 pt-3 text-secondary">
+            Women Voice 2020 @ All right reserved
+          </li>
+        </ul>
+      </footer>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
