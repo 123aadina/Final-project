371f50ff4a5e3917fb0ab26617be8c3f08f2bd3d
Christophe Troudart <ch.troudart@gmail.com>
2020-03-25 13:24:42 +0100

    Topic page created for the list ofissues where we fournish informations

---
+++ front-end/src/components/Pages/Topic.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Footer from "../Layout/Footer";
+import "../styles/App.scss";
+
+export default function Topic() {
+  return <div className="topic_container"></div>;
+}
