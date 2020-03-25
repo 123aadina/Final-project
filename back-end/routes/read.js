97ab2f8825bfa305ac7f03f1a45356ea30b26641
123aadina <adina.hamida@gmail.com>
2020-03-25 16:40:14 +0100

    some file commented and some basic changing into the files

--- back-end/routes/read.js
+++ back-end/routes/read.js
@@ -4,6 +4,39 @@ const models = require("../models");
 const Organisation = models.Organisation;
 const Problem = models.Problem;
 
+
+//Get all orgnations
+router.get("/organisation", (req, res) => {
+  Organisation.find().then(organisations => {
+    res.send(organisations);
+  });
+});
+
+//Get all problems
+router.get("/problem", (req, res) => {
+  Problem.find()
+  //.catch(err => res.send(err))
+  .then(problem => {
+    res.send(problem);
+  });
+});
+
+
+//Get all organisations according problems ids
+router.get("/problem/:id/organisations", (req, res) => {
+  const problemId = req.params.id
+  console.log(problemId)
+  Problem.findById(problemId).populate('organisations')
+  .catch(err => res.send(err))
+  .then(problem => {
+    res.send(problem.organisations);
+  });
+});
+
+module.exports = router;
+
+
+
 /*const User = models.User;
 const Translation = models.Translation;*/
 
@@ -24,36 +57,6 @@ router.get("/translations", (req, res, next) => {
     res.send(translations);
   });
 });*/
-
-
-//Get all orgnations
-router.get("/organisation", (req, res) => {
-  Organisation.find().then(organisations => {
-    res.send(organisations);
-  });
-});
-
-//Get all problems
-router.get("/problem/:id/organisations", (req, res) => {
-  const problemId = req.params.id
-  console.log(problemId)
-  Problem.findById(problemId).populate('organisations')
-
-  .catch(err => res.send(err))
-  .then(problem => {
-    res.send(problem.organisations);
-  });
-});
-
-router.get("/problem", (req, res) => {
-  Problem.find()
-  //.catch(err => res.send(err))
-  .then(problem => {
-    res.send(problem);
-  });
-});
-
-
 /*
 router.put('/:id', (req, res) => {
   res.send(' update problems')
@@ -63,4 +66,4 @@ router.delete('/:id', (req, res) => {
   res.send(' delete problems')
 })*/
 
-module.exports = router;
+
