e5e5a80045019b192724c46e4db84bb63d0318f8
Christophe Troudart <ch.troudart@gmail.com>
2020-03-25 21:17:12 +0100

    Functionnal component create for the Topic page

--- front-end/src/components/Pages/Topic.js
+++ front-end/src/components/Pages/Topic.js
@@ -3,5 +3,9 @@ import Footer from "../Layout/Footer";
 import "../styles/App.scss";
 
 export default function Topic() {
-  return <div className="topic_container"></div>;
+  return (
+    <div className="topic_container">
+      <h1>Topics</h1>
+    </div>
+  );
 }
