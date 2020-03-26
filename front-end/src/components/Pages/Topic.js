deceaf7754fe4dbbdc9cda368a6695a82e2ec675
Christophe Troudart <ch.troudart@gmail.com>
2020-03-26 12:50:38 +0100

    CContainer for the topic box added

--- front-end/src/components/Pages/Topic.js
+++ front-end/src/components/Pages/Topic.js
@@ -7,6 +7,44 @@ export default function Topic() {
   return (
     <div className="topic_container">
       <h1>Topics</h1>
+
+      <div className="topic_container_box ">
+        <div className="row">
+          <div className="btn btn-light col-6">
+            <Link>Childcare </Link>
+          </div>
+        </div>
+        <div className="row">
+          <div className="btn btn-light col-6">
+            {" "}
+            <Link>Employement</Link>{" "}
+          </div>
+        </div>
+        <div className="row">
+          <div className="btn btn-light col-6">
+            {" "}
+            <Link>Divorce</Link>{" "}
+          </div>
+        </div>
+        <div className="row">
+          <div className="btn btn-light col-6">
+            <Link>Domestic Violence </Link>{" "}
+          </div>
+        </div>
+        <div className="row">
+          <div className="btn btn-light col-6">
+            {" "}
+            <Link>Healthcare</Link>{" "}
+          </div>
+        </div>
+        <div className="row">
+          <div className="btn btn-light col-6">
+            {" "}
+            <Link>Sexual Abuse</Link>{" "}
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
