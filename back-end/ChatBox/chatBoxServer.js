2c0f1d2a71e897ad01d387a8d56b74daf0bf6549
marnanakou <marnanakou@gmail.com>
2020-03-31 16:24:16 +0200

    api and socket middleware

--- back-end/ChatBox/chatBoxServer.js
+++ back-end/ChatBox/chatBoxServer.js
@@ -1,14 +1,39 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(server);
-const users = {};
+const io = require("socket.io")(http);
+const jtw = require("jsonwebtoken");
+
+//setting middleware for the api and socket 
+api.use((req, res, next) => {
+  // res.set sets an HTTP header on the response
+  // these two headers grant EVERYBODY access to our API
+  res.set('ACCESS-CONTROL-ALLOW-ORIGIN', '*');
+  res.set('ACCESS-CONTROL-ALLOW-HEADERS', '*');
+  res.set('ACCESS-CONTROL-ALLOW-METHODS', '*');
+  next();
+
+app.use(express.json());
+io.use((socket, next) => {
+  const token = socket.handshake.query.token;
+});
+
+//verifying token(just to remember it expires after 2h)
+jtw.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+  if (err) {
+    return next(err);
+  } else {
+    //if user is verified we store users.id on the users socket and then next() to let them connect
+    socket._id = decoded._id;
+    next();
+  }
+});
 
 //setting connection between server and client
 io.on("connection", function(socket) {
   console.log("User is connected");
   socket.on("chat message", function(msg) {
     console.log("message:" + JSON.stringify(msg));
-    //sending to all clients
+    //broadcasting sending to all clients
     io.emit("chat message", msg);
   });
   socket.on("disconnect", function(user) {
@@ -24,6 +49,8 @@ http.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-//sto chatbox tha mporei o kathenas na milisei me kapoion i tha prepei na einai eggegramenos xristis?
-//socket must listen to an event called connection
-//xreiazomai routes? i afou exoume to socket den xreiazomaste kati allo?
+//prepei kathe xristis na exei to diko tou kanali kai na xei prosvasi o xristis kai oi administrators
+
+//prepei o administrator na exei prosvasi se ola ta kanalia
+
+//1.authodicate user kai create a unique connection
