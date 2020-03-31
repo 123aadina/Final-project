a149c235b09f119b3042eb6fff551d678bfc8c47
Christophe Troudart <ch.troudart@gmail.com>
2020-03-31 23:15:55 +0100

    Conflict solved

--- front-end/src/components/Pages/Topic.js
+++ front-end/src/components/Pages/Topic.js
@@ -9,46 +9,22 @@ export default function Topic() {
       <h1 className="text-center">Topics</h1>
 
       <div className="topic_container_box ">
-<<<<<<< HEAD
-        <div className="col">
-          <div className="btn btn-light col-6">
-            <Link>Childcare </Link>
-          </div>
-        </div>
-        <div className="col">
-          <div className="btn btn-light col-6">
-=======
         <div className="row">
           <div className="btn btn-light col-6 mx-auto">
             <Link>Childcare </Link>
           </div>
 
           <div className="btn btn-light col-6 mx-auto">
->>>>>>> 273c64a9eedb960785fa4872e0dc1fae8f811cde
             {" "}
             <Link to="/">Employement</Link>{" "}
           </div>
         </div>
-<<<<<<< HEAD
-        <div className="col">
-          <div className="btn btn-light col-6">
-=======
         <div className="row">
           <div className="btn btn-light col">
->>>>>>> 273c64a9eedb960785fa4872e0dc1fae8f811cde
             {" "}
             <Link to="/">Divorce</Link>{" "}
           </div>
         </div>
-<<<<<<< HEAD
-        <div className="col">
-          <div className="btn btn-light col-6">
-            <Link>Domestic Violence </Link>{" "}
-          </div>
-        </div>
-        <div className="col">
-          <div className="btn btn-light col-6">
-=======
         <div className="row">
           <div className="btn btn-light col">
             <Link to="/">Domestic Violence </Link>{" "}
@@ -56,18 +32,12 @@ export default function Topic() {
         </div>
         <div className="row">
           <div className="btn btn-light col">
->>>>>>> 273c64a9eedb960785fa4872e0dc1fae8f811cde
             {" "}
             <Link to="/">Healthcare</Link>{" "}
           </div>
         </div>
-<<<<<<< HEAD
-        <div className="col">
-          <div className="btn btn-light col-6">
-=======
         <div className="row">
           <div className="btn btn-light col">
->>>>>>> 273c64a9eedb960785fa4872e0dc1fae8f811cde
             {" "}
             <Link to="/">Sexual Abuse</Link>{" "}
           </div>
