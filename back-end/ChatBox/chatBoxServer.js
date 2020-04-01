929a44c9677ef5cce511b5c26c00e643c2c6e969
marnanakou <marnanakou@gmail.com>
2020-04-01 11:10:04 +0200

    changes to commit

--- back-end/ChatBox/chatBoxServer.js
+++ back-end/ChatBox/chatBoxServer.js
@@ -3,29 +3,19 @@ const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 const jtw = require("jsonwebtoken");
 
-//setting middleware for the api and socket 
+//setting middleware for the api and socket
 api.use((req, res, next) => {
   // res.set sets an HTTP header on the response
   // these two headers grant EVERYBODY access to our API
-  res.set('ACCESS-CONTROL-ALLOW-ORIGIN', '*');
-  res.set('ACCESS-CONTROL-ALLOW-HEADERS', '*');
-  res.set('ACCESS-CONTROL-ALLOW-METHODS', '*');
+  res.set("ACCESS-CONTROL-ALLOW-ORIGIN", "*");
+  res.set("ACCESS-CONTROL-ALLOW-HEADERS", "*");
+  res.set("ACCESS-CONTROL-ALLOW-METHODS", "*");
   next();
+});
 
 app.use(express.json());
 io.use((socket, next) => {
   const token = socket.handshake.query.token;
-});
-
-//verifying token(just to remember it expires after 2h)
-jtw.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
-  if (err) {
-    return next(err);
-  } else {
-    //if user is verified we store users.id on the users socket and then next() to let them connect
-    socket._id = decoded._id;
-    next();
-  }
 });
 
 //setting connection between server and client
@@ -41,16 +31,25 @@ io.on("connection", function(socket) {
   });
 });
 
-//receiving and updating messages
+//verifying token(just to remember it expires after 2h)
+jtw.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+  if (!user) {
+    return next(err);
+  } else {
+    //if user is verified we store users.id on the users socket and then next() to let them connect
+    socket._id = decoded._id;
+    next();
+  }
+});
 
-//http listening on
+//http listening on port 8000??ask Robert
 const port = 8000;
 http.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-//prepei kathe xristis na exei to diko tou kanali kai na xei prosvasi o xristis kai oi administrators
-
-//prepei o administrator na exei prosvasi se ola ta kanalia
-
 //1.authodicate user kai create a unique connection
+//receiving and updating messages?
+//the port should be different ?ASK Robert
+//prepei kathe xristis na exei to diko tou kanali kai na xei prosvasi o xristis kai oi administrators
+//prepei o administrator na exei prosvasi se ola ta kanalia
