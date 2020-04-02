a8676fe2ca30227e0c0ea1c055a2c3ee09311008
123aadina <adina.hamida@gmail.com>
2020-04-02 17:43:20 +0200

    some listItem, list and listItemText add from @matrialul

--- front-end/src/components/ChatApp/Dashboard.js
+++ front-end/src/components/ChatApp/Dashboard.js
@@ -5,6 +5,10 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 //@material-ui/styles
 import { makeStyles } from '@material-ui/core/styles';
+//@material-ui/ListItem/list/ListItemText
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const useStyles = makeStyles((theme) => ({
     root: {
