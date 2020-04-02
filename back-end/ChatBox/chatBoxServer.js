728aa7f8b9883728d3146017ed960f7318eb5a8a
marnanakou <marnanakou@gmail.com>
2020-04-02 10:34:12 +0200

    setting connection with web socket

--- back-end/ChatBox/chatBoxServer.js
+++ back-end/ChatBox/chatBoxServer.js
@@ -1,55 +1,50 @@
-const app = require("express")();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-const jtw = require("jsonwebtoken");
+module.exports = server => {
+  const io = require("socket.io")(server);
 
-//setting middleware for the api and socket
-api.use((req, res, next) => {
-  // res.set sets an HTTP header on the response
-  // these two headers grant EVERYBODY access to our API
-  res.set("ACCESS-CONTROL-ALLOW-ORIGIN", "*");
-  res.set("ACCESS-CONTROL-ALLOW-HEADERS", "*");
-  res.set("ACCESS-CONTROL-ALLOW-METHODS", "*");
-  next();
-});
+  // const jtw = require("jsonwebtoken");
+  // const router = express.Router();
 
-app.use(express.json());
-io.use((socket, next) => {
-  const token = socket.handshake.query.token;
-});
+  // io.use((socket, next) => {
+  //   const token = socket.handshake.query.token;
+  // });
 
-//setting connection between server and client
-io.on("connection", function(socket) {
-  console.log("User is connected");
-  socket.on("chat message", function(msg) {
-    console.log("message:" + JSON.stringify(msg));
-    //broadcasting sending to all clients
-    io.emit("chat message", msg);
+  //setting connection between server and client
+  io.on("connection", function(socket) {
+    console.log("User is connected");
+
+    socket.on("chat message", function(msg) {
+      console.log("message:" + JSON.stringify(msg));
+      //broadcasting sending to all clients
+      io.emit("chat message", msg);
+      //edw o administrator tha prepei na lamvanei eidopoiisi oti exei ginei to connection gia na
+    });
+    socket.on("disconnect", function(user) {
+      io.emit("User has left the chat");
+    });
   });
-  socket.on("disconnect", function(user) {
-    io.emit("User has left the chat");
-  });
-});
 
-//verifying token(just to remember it expires after 2h)
-jtw.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
-  if (!user) {
-    return next(err);
-  } else {
-    //if user is verified we store users.id on the users socket and then next() to let them connect
-    socket._id = decoded._id;
-    next();
-  }
-});
+  // //verifying token(just to remember it expires after 2h)
+  // jtw.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
+  //   if (!user) {
+  //     return next(err);
+  //   } else {
+  //     //if user is verified we store users.id on the users socket and then next() to let them connect
+  //     socket._id = decoded._id;
+  //     next();
+  //   }
+  // });
 
-//http listening on port 8000??ask Robert
-const port = 8000;
-http.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  // //http listening on port 8000??ask Robert
+  // const port = 8000;
+  // http.listen(port, () => {
+  //   console.log(`Listening on port ${port}`);
+  // });
 
-//1.authodicate user kai create a unique connection
-//receiving and updating messages?
-//the port should be different ?ASK Robert
-//prepei kathe xristis na exei to diko tou kanali kai na xei prosvasi o xristis kai oi administrators
-//prepei o administrator na exei prosvasi se ola ta kanalia
+  // module.exports = router;
+
+  //receiving and updating messages?
+  //gia kathe xristi prepei na iparxei diaforetiko kanali tha to kanoume me to username gia na dimiourgisoume room kai meta tha anoixei to kanali
+  //var room = name
+  //validation gia ta kanaliacd
+  //prepei o administrator na exei prosvasi se ola ta kanalia
+};
