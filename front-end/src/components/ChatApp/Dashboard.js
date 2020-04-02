e00dd5648e7c6f3b914bf41148539b454ae104e9
123aadina <adina.hamida@gmail.com>
2020-04-02 17:00:11 +0200

    margin add to the dashboard

--- front-end/src/components/ChatApp/Dashboard.js
+++ front-end/src/components/ChatApp/Dashboard.js
@@ -8,6 +8,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
+        margine:'50px',
       
       
         margin: theme.spacing(1),
