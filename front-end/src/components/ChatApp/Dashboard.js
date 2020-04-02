724cdb23297c534dae78856ef58efc35018bae21
123aadina <adina.hamida@gmail.com>
2020-04-02 16:25:20 +0200

    @material-ui/Typography add to the Dashboard

--- front-end/src/components/ChatApp/Dashboard.js
+++ front-end/src/components/ChatApp/Dashboard.js
@@ -1,10 +1,13 @@
 import React from 'react'
-//@material-ui
+//@material-ui/paper
 import Paper from '@material-ui/core/Paper';
+//@material-ui/Typography
+import Typography from '@material-ui/core/Typography';
 
 const Dashboard = () => {
     return (
         <div>
+             
             
         </div>
     )
