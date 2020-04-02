66b6468e22ab073650687fc4109dc26f831315f6
123aadina <adina.hamida@gmail.com>
2020-04-02 16:58:47 +0200

    display flex is deleted

--- front-end/src/components/ChatApp/Dashboard.js
+++ front-end/src/components/ChatApp/Dashboard.js
@@ -8,7 +8,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      display: 'flex',
+      
       
         margin: theme.spacing(1),
        
