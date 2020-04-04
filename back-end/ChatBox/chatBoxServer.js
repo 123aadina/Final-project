9faa4202b6c3770a1e1d475963c0ea245e2f66f9
marnanakou <marnanakou@gmail.com>
2020-04-04 11:29:01 +0200

    establish admin connection

--- back-end/ChatBox/chatBoxServer.js
+++ back-end/ChatBox/chatBoxServer.js
@@ -25,6 +25,7 @@ module.exports = server => {
       //broadcasting to all clients
       io.emit(room, msg);
     });
+
     // Disconnect from the room
     socket.on("disconnect", function(msg) {
       io.emit("User has left the chat");
@@ -32,29 +33,9 @@ module.exports = server => {
 
     // Joining the room
     socket.join(room);
+    //notify the admin that there is a message from user and in which room
+    io.emit("userSentMessage", { roomName: room });
     // Sending welcome message to the connected user
     io.emit(room, "Welcome to the chat!");
   });
-
-  //Creating different end points on the client side(FOR FRONTEND)
-  // const socket = io('/my-namespace');
-
-  // //creating the namespace for assigning different end points
-  // const nsp = io.of("/my-namespace");
-  // nsp.on("connection", function(socket) {
-  //   console.log("message:" + JSON.stringify(msg)); //
-
-  // //when user leaves the chat
-  // nsp.on("leave", function(socket) {
-  //   console.log("User has left the chat");
-  // });
-
-  // const jtw = require("jsonwebtoken");
-  // const router = express.Router();
-
-  // io.use((socket, next) => {
-  //   const token = socket.handshake.query.token;
-  // });
-
-  //receiving and updating messages?
 };
