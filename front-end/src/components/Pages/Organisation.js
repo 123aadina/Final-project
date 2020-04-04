6213250cdffe3844ba7c36b53e493962eeb5d7da
Christophe Troudart <ch.troudart@gmail.com>
2020-04-04 15:32:18 +0100

    Components navbar et footer imported

--- front-end/src/components/Pages/Organisation.js
+++ front-end/src/components/Pages/Organisation.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+// COMPONENTS
+import Navbar from "../Layout/Navbar";
+import Footer from "../Layout/Footer";
+
 export default function Organisation() {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+
+      <Footer />
+    </div>
+  );
 }
