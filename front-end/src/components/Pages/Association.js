5d34d8efeaadd3ad52b60a382f7bc274742704d1
Christophe Troudart <ch.troudart@gmail.com>
2020-04-05 17:59:43 +0100

    H1 created for the association page

--- front-end/src/components/Pages/Association.js
+++ front-end/src/components/Pages/Association.js
@@ -6,9 +6,9 @@ import Footer from "../Layout/Footer";
 
 export default function Association() {
   return (
-    <div>
+    <div className="container d-flex justify-content-center flex-wrap flex-column">
       <Navbar />
-
+      <h1 className="text-center "> They can help you </h1>
       <Footer />
     </div>
   );
