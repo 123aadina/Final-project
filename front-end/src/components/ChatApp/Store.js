0a8014e729c04a5534ac9b9504b559897207068f
123aadina <adina.hamida@gmail.com>
2020-04-06 15:56:32 +0200

    reducer and inintial State add

--- front-end/src/components/ChatApp/Store.js
+++ front-end/src/components/ChatApp/Store.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const CTX = React.createContext()
 
 const Store = () => {
-
+    const reducrHook = React.useReducer(reducer, initialState)
     return (
         <CTX.Provider value={}>
             {props.children}
