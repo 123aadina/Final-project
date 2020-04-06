eebad801e1f830660e93269d2a3a06a702304c9b
marnanakou <marnanakou@gmail.com>
2020-04-06 15:21:36 +0200

    emailMessage and transporter to send messages

--- back-end/SendConfigurationEmail/emailSend.js
+++ back-end/SendConfigurationEmail/emailSend.js
@@ -3,7 +3,24 @@ const nodemailer = require("nodemailer");
 
 //create transporter to deliver the messages
 const transporter = nodemailer.createTransport({
-    host:,
+    host:,//email goes here
     port:,
-    auth:
+    auth:{}
+})
+
+var emailMessage = {
+    from: "sender@server.com",
+    to: "receiver@sender.com",
+    subject: "Message title",
+    text: "Please click on this link to confirm your registration",
+    html: "<p>HTML version of the message</p>"
+  };
+
+//and since now we have the transporter send emails
+transporter.sendMail(emailMessage,(err,data)=>{
+    if (err){
+        res.json({msg:'fail'})
+    } else {
+        res.json ({msg:})
+    }
 })
