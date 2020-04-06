e0b0d66690170059518804e3cb671d6d8e7054b9
123aadina <adina.hamida@gmail.com>
2020-04-06 16:00:04 +0200

    funtion reducer created and completed

--- front-end/src/components/ChatApp/Store.js
+++ front-end/src/components/ChatApp/Store.js
@@ -2,6 +2,22 @@ import React from 'react'
 
 export const CTX = React.createContext()
 
+function reducer(state, action) {
+    const { msg, from, topic } = action.payload
+    switch (action.type) {
+        case 'RECEIVE_MESSAGE':
+            return {
+                ...state,
+                [topic]: [
+                    ...state[topic],
+                    { from, msg }
+                ]
+            }
+        default:
+            return state
+    }
+}
+
 const Store = props => {
     const reducrHook = React.useReducer(reducer, initialState)
     return (
