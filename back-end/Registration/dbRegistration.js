7eb1a548e1270a28a722a34141cd030e577f05dd
marnanakou <marnanakou@gmail.com>
2020-04-09 15:17:40 +0200

    adding chat and chatRoom in schema

--- back-end/Registration/dbRegistration.js
+++ back-end/Registration/dbRegistration.js
@@ -6,16 +6,18 @@ const User = mongoose.model(
   "User",
   new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: false, unique: true },
+    email: { type: String, required: false },
     password: { type: String, required: true },
     phone: { type: String, required: true },
     issues: { type: Number, required: true },
-    languages: { type: String, required: false },
+    languages: { type: [String], required: false },
     comment: { type: String, required: false },
     emailChecked: { type: Boolean, required: false },
     agreeChecked: { type: Boolean, required: true },
     //to distinguish if a user is an administrator or not for the chat
     isAdministrator: { type: Boolean, required: true, default: false },
+    chatRoom: { type: String, required: false },
+    chat: { type: Boolean, default: false, required: true },
   })
 );
 
