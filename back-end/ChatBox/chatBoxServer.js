65f1f31aa04ff9994e26f8e054e3ab5791818176
marnanakou <marnanakou@gmail.com>
2020-04-15 12:12:45 +0200

    lets see if it is working

--- back-end/ChatBox/chatBoxServer.js
+++ back-end/ChatBox/chatBoxServer.js
@@ -5,27 +5,41 @@ module.exports = (server) => {
   // setup connection and events
   io.on("connection", (socket) => {
     console.log("Connection established");
-    socket.on("joinRoom", ({ room, username }) => {
+
+    socket.on("joinRoom", ({ username }) => {
       socket.join(room);
-      console.log(`User ${username} joined room ${room}`);
+      User.findOneAndUpdate({ name: username }, { chatRoom: username }).catch(
+        (error) => {
+          console.log(error);
+        }
+      );
+
+      // console.log(`User ${username} joined room ${room}`);
       // inform users in the room that a new user joined
-      socket.emit("message", `User ${username} joined room ${room}`);
+      // socket.emit("message", `User ${username} joined room ${room}`);
     });
+    socket.on("rooms", (username) => {
+      console.log("listOfRooms");
+      User.findOne({ isAdministrator: true, name: username }).then((user) => {
+        if (user) {
+          User.find()
+            .select("name")
+            .then((users) => {
+              io.emit("rooms", [user]);
+            });
+        } else {
+          io.emit("rooms", [username]);
+        }
+      });
+    });
+
     // listen to incoming messages
-    socket.on("message", ({ room, msg }) => {
-      console.log(`Room ${room}: ${msg}`);
+    socket.on("message", ({ username, msg }) => {
+      console.log(`Room ${username}: ${msg}`);
       // broadcast ALL - send message to everybody INCLUDING me
-      io.to(room).emit("message", `Room ${room}: ${msg}`);
+      io.to(username).emit("message", msg);
       // broadcast - send message to everybody EXCLUDING me
       // socket.broadcast.to(room).emit("message", `Room ${room}: ${msg}`)
-    });
-    socket.on("rooms", (userId) => {
-      console.log("listOfRooms");
-      User.find({ chat: true })
-        .select(["name", "_id", "chat", "chatRoom"])
-        .then((users) => {
-          io.emit("message", users);
-        });
     });
   });
 };
