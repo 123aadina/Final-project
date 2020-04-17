f9a1d5a0fa05a58c5b30ab192c5f9d81add99612
marnanakou <marnanakou@gmail.com>
2020-04-17 14:24:11 +0200

    import styling

--- front-end/src/components/Registration/BallonBox.js
+++ front-end/src/components/Registration/BallonBox.js
@@ -1,17 +1,29 @@
 import React from "react";
+// Styling
+import "../../styles/App.scss";
 
 const BallonBox = (props) => {
   return (
     <div
-      style={{
-        borderRadius: "5px",
-        position: "relative",
-        width: "300px",
-        height: "100px",
-      }}
+      //   style={{
+      //     borderRadius: "5px",
+      //     position: "relative",
+      //     width: "300px",
+      //     height: "100px",
+      //   }}
       className="ballonBox"
     >
-      <div style={{ textAlign: "center" }} className="ballonBox-input">
+      <div
+        // style={{
+        //   textAlign: "center",
+        //   position: "absolute",
+        //   width: "0",
+        //   height: "0",
+        //   borderColor: "#34495E",
+        //   content: "",
+        // }}
+        className="ballonBox-input"
+      >
         <h3>{props.title}</h3>
       </div>
     </div>
