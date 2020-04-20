5c109d76760367a92365bb632b439d0aa2994702
marnanakou <marnanakou@gmail.com>
2020-04-20 16:23:52 +0200

    change email to required

--- back-end/Registration/dbRegistration.js
+++ back-end/Registration/dbRegistration.js
@@ -5,8 +5,8 @@ const Schema = mongoose.Schema;
 const User = mongoose.model(
   "User",
   new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-    email: { type: String, required: false },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     phone: { type: String, required: true },
     issues: { type: Number, required: true },
