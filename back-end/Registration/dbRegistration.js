9074460c8e4b130f6c31e47a04b07123432ee712
marnanakou <marnanakou@gmail.com>
2020-04-20 15:05:44 +0200

    making name as unique

--- back-end/Registration/dbRegistration.js
+++ back-end/Registration/dbRegistration.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const User = mongoose.model(
   "User",
   new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
     email: { type: String, required: false },
     password: { type: String, required: true },
     phone: { type: String, required: true },
