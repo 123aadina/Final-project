0c01ef5db939eee57619d1d43477a738588ec7eb
Christophe Troudart <ch.troudart@gmail.com>
2020-04-24 14:16:13 +0100

    Second Home removed from the footer

--- front-end/src/components/Layout/Footer.js
+++ front-end/src/components/Layout/Footer.js
@@ -38,13 +38,6 @@ const Footer = () => {
             {t("Topics.1")}{" "}
           </Link>
         </li>
-
-        <li className="mx-2 pt-3 ">
-          <Link to="/association" className="text-secondary">
-            {" "}
-            {t("home.1")}{" "}
-          </Link>
-        </li>
       </ul>
 
       <div className="text-secondary text-center">
