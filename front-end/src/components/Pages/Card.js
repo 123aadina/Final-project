0bd98f9b4570636f13bf5225d1897e65c55fc6fb
Christophe Troudart <ch.troudart@gmail.com>
2020-04-27 08:36:30 +0100

    col modified

--- front-end/src/components/Pages/Card.js
+++ front-end/src/components/Pages/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Card(props) {
   return (
-    <div className="card mx-2 my-2 col-3-xs col-4-sm">
-      <div className="card-body text-center">
+    <div className="card mx-2 my-2 col-3-xs col-3-sm">
+      <div className="card-body text-center ">
         <h5 className="card-title text-center "> {props.name}</h5>
         <p className="card-text"> {props.description}</p>
         <button className="btn btn-primary text-center">
