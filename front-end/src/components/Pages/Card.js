cbca7e31f48449effece22695eed11cc5784b3d2
Christophe Troudart <ch.troudart@gmail.com>
2020-04-27 11:09:53 +0100

    Fixing layout

--- front-end/src/components/Pages/Card.js
+++ front-end/src/components/Pages/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Card(props) {
   return (
-    <div className="card mx-2 my-2 col-3-xs col-3-sm">
+    <div className="card mx-2 my-2 col-3-xs col-3-sm problems_cards">
       <div className="card-body text-center">
         <h5 className="card-title text-center "> {props.name}</h5>
         <p className="card-text"> {props.description}</p>
