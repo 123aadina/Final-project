1e46afeed2a5e63807a9c559cc424d0c1cd35e25
Christophe Troudart <ch.troudart@gmail.com>
2020-04-27 17:59:57 +0100

    Component navbar removed

--- front-end/src/components/Pages/Topic.js
+++ front-end/src/components/Pages/Topic.js
@@ -2,9 +2,6 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
-// Components
-import Navbar from "../Layout/Navbar";
-
 export default function Topic() {
   const { t, i18n } = useTranslation();
   const [problems, changeProblems] = useState([]);
@@ -22,7 +19,6 @@ export default function Topic() {
       className="container d-flex flex-column justify-content-center col"
       id="topic_container"
     >
-      <Navbar />
       <h1 className="text-center"> Topics </h1>
 
       <div
@@ -32,7 +28,10 @@ export default function Topic() {
         <div className="row">
           {problems.map((problem, id) => {
             return (
-              <div key={id} className="col-6 mx-auto text-center text-uppercase  ">
+              <div
+                key={id}
+                className="col-6 mx-auto text-center text-uppercase  "
+              >
                 <div className="card m-2 bg-white rounded shadow p-3">
                   <Link
                     className="text-decoration-none text-darker justify-content-center text-dark"
